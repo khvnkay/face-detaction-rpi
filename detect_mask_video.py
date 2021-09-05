3f6e67a138a9e61a7828b2c7fd7000b09f5c0373
khvnkay <khvnkay@gmail.com>
2021-09-05 12:18:44 +0700

    show image

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -121,38 +121,37 @@ while True:
 
 	# detect faces in the frame and determine if they are wearing a
 	# face mask or not
-	(locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
+	# (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
 
-	# loop over the detected face locations and their corresponding
-	# locations
-	for (box, pred) in zip(locs, preds):
-		# unpack the bounding box and predictions
-		(startX, startY, endX, endY) = box
-		(mask, withoutMask) = pred
+	# # loop over the detected face locations and their corresponding
+	# # locations
+	# for (box, pred) in zip(locs, preds):
+	# 	# unpack the bounding box and predictions
+	# 	(startX, startY, endX, endY) = box
+	# 	(mask, withoutMask) = pred
 
-		# determine the class label and color we'll use to draw
-		# the bounding box and text
-		if mask > withoutMask:
-			label = "Mask"
-			print("Mask")
-		else:
-			label = "No Mask"
-			print("No Mask")
-		# label = "Mask" if mask > withoutMask else "No Mask"
-		color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+	# 	# determine the class label and color we'll use to draw
+	# 	# the bounding box and text
+	# 	if mask > withoutMask:
+	# 		label = "Mask"
+	# 		print("Mask")
+	# 	else:
+	# 		label = "No Mask"
+	# 		print("No Mask")
+	# 	# label = "Mask" if mask > withoutMask else "No Mask"
+	# 	color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
 			
-		# include the probability in the label
-		label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+	# 	# include the probability in the label
+	# 	label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
-		# display the label and bounding box rectangle on the output
-		# frame
-		cv2.putText(frame, label, (startX, startY - 10),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+	# 	# display the label and bounding box rectangle on the output
+	# 	# frame
+	# 	cv2.putText(frame, label, (startX, startY - 10),
+	# 		cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+	# 	cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
 
 	# show the output frame
-	camera.capture(rawCapture, format="bgr")
-	frame = rawCapture.array
+	
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
 
