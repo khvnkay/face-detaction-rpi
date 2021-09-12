8188fd63abca00d41daf29117909385f4979ffba
khvnkay <khvnkay@gmail.com>
2021-09-12 10:13:54 +0700

    update

--- facial_req.py
+++ facial_req.py
@@ -78,7 +78,7 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 	# grab the dimensions of the frame and then construct a blob
 	# from it
 	(h, w) = frame.shape[:2]
-	blob = cv2.dnn.a(frame, 1.0, (300, 300),
+	blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
 		(104.0, 177.0, 123.0))
 
 	# pass the blob through the network and obtain the face detections
