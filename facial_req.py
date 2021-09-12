678b50e62e3a97119cdc96fb9c22c025597c45d9
khvnkay <khvnkay@gmail.com>
2021-09-12 11:05:00 +0700

    test rgb

--- facial_req.py
+++ facial_req.py
@@ -79,9 +79,10 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 	# grab the dimensions of the frame and then construct a blob
 	# from it
 	(h, w) = frame.shape[:2]
-	blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
-		(104.0, 177.0, 123.0))
-	print("start----1")
+	# blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
+	# 	(104.0, 177.0, 123.0))
+	blob = cv2.dnn.blobFromImage(image, size=(300, 300), swapRB=True)
+	print("start----1",blob)
 	# pass the blob through the network and obtain the face detections
 	faceNet.setInput(blob)
 	detections = faceNet.forward()
